4c9d4141500c3b4684aface117daec0a7de734b0
7assan-5alloof <hassankhalloof20@gmail.com>
2021-04-13 04:30:16 +0400

    Write program (count of results should be 18, is 19 - all results are just the number itself)

--- main.cpp
+++ main.cpp
@@ -2,30 +2,13 @@
 
 using namespace std;
 
-int numberOfPrimes(int);
 int main()
 {
     int number; cout << "Enter a number >= 2: "; cin >> number; // Input from user
-    int length = numberOfPrimes(number); // Store needed length for array
-    int myprime[length]; // Will specify length of array as needed as per user's input
 
-    // Find values to add to array
-
-
-    // Display results
-    for (int i = 0; i < length; i++)
-    {
-        cout << myprime[i];
-        if (i + 1 < length) cout << " ";
-    }
-
-    return 0;
-}
-
-int numberOfPrimes(int input)
-{
-    int result;
-    for (int i = 2; i < input; i++)
+    // Find number of values to add to array
+    int cnt = 0;
+    for (int i = 2; i <= number; i++)
     {
         bool prime = true;
         for (int j = 2; j < i; j++)
@@ -37,8 +20,32 @@ int numberOfPrimes(int input)
                 break;
             }
         }
-        if (prime) result++;
+        if (prime) cnt++;
     }
 
-    return result;
+    // Run until the expected number of values expected is reached, and add to array along the way
+    int myprime[cnt];
+    for (int i = 0; i < cnt; i++) for (int j = 2; j <= number; j++)
+        {
+            bool prime = true;
+            for (int k = 2; k < j; k++)
+            {
+                if (j == k) continue;
+                if (j % k == 0)
+                {
+                    prime = false;
+                    break;
+                }
+            }
+            if (prime) myprime[i] = j;
+        }
+
+    // Display results
+    for (int i = 0; i < cnt; i++)
+    {
+        cout << myprime[i];
+        if (i + 1 < cnt) cout << " ";
+    }
+
+    return 0;
 }
